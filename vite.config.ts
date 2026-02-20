0166bbd43a199f1bc2b984a2f7fe7db8f1ca8255
Aryan_0854 <133648351+Aryan0854@users.noreply.github.com>
2026-02-20 14:31:09 +0530

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Update this to match your repository name
+  base: '/', // Update this to match your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
